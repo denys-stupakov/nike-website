6cc1afdaade7f295f8c87cf78139209ed3ca0f50
Denys Stupakov <stupakov106@gmail.com>
2025-08-18 16:36:11 +0200

    Refactor App component layout and add utility styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
+const App = () => {
+
+
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        Hero
+      </section>
+      <section className="padding">
+        PopularProducts
+      </section>
+      <section className="padding">
+        SuperQuality
+      </section>
+      <section className="padding-x py-10">
+        Services
+      </section>
+      <section className="padding">
+        SpecialOffer
+      </section>
+      <section className="padding bg-pale-blue">
+        CustomerReviews
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        Subscribe
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        Footer
+      </section>
+    </main>
   )
 }
 
