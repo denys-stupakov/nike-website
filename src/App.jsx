dd7cd12ecce11b9ea9a786478737a7619dae2a7b
Denys Stupakov <stupakov106@gmail.com>
2025-08-18 16:57:11 +0200

    Add Services section component and update App component imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Hero from './sections/Hero'
+import PopularProducts from './sections/PopularProducts'
+import SuperQuality from './sections/SuperQuality'
+import Services from './sections/Services'
+import SpecialOffer from './sections/SpecialOffer'
+import CustomerReviews from './sections/CustomerReviews'
+import Subscribe from './sections/Subscribe'
+import Footer from './sections/Footer'
 
 const App = () => {
 
