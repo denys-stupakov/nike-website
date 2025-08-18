31d2eef8bdd98023f1a97bec159d978bac3b2599
Denys Stupakov <stupakov106@gmail.com>
2025-08-18 17:39:57 +0200

    Update index.html title and favicon path; implement Nav component structure

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,25 @@
+import headerLogo from "../assets/images/header-logo.svg";
+import hamburger from "../assets/icons/hamburger.svg";
+
 const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
+    return (
+        <>
+            <header className="padding-x py-8 absolute z-10 w-full">
+                <nav className="flex justify-between items-center max-container">
+                    <a href="/">
+                        <img
+                            src={headerLogo}
+                            alt="Logo"
+                            width={130} height={29}
+                        />
+                    </a>
+                    <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+                        <li></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
 }
 
 export default Nav
