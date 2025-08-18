28fda13800c11bf3b5c3b32af2b39306fedbbb66
Denys Stupakov <stupakov106@gmail.com>
2025-08-18 18:04:47 +0200

    Fix hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,11 @@
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section
+      id="home" 
+      className="flex flex-col xl:flex-row justify-center min-h-screen gap-10"
+    >
+
+    </section>
   )
 }
 
