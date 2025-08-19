c88a1dc5d2bacfb5c5c01ef24b5abdfc0888ba95
Denys Stupakov <stupakov106@gmail.com>
2025-08-19 14:46:55 +0200

    Finish the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -11,37 +11,35 @@ import Subscribe from './sections/Subscribe'
 import Footer from './sections/Footer'
 
 const App = () => {
-
-
   return (
-    <main className="relative">
+    <main className='relative'>
       <Nav />
-      <section className="xl:padding-l wide:padding-r padding-b">
+      <section className='xl:padding-l wide:padding-r padding-b'>
         <Hero />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <PopularProducts />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <SuperQuality />
       </section>
-      <section className="padding-x py-10">
+      <section className='padding-x py-10'>
         <Services />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <SpecialOffer />
       </section>
-      <section className="padding bg-pale-blue">
+      <section className='bg-pale-blue padding'>
         <CustomerReviews />
       </section>
-      <section className="padding-x sm:py-32 py-16 w-full">
+      <section className='padding-x sm:py-32 py-16 w-full'>
         <Subscribe />
       </section>
-      <section className="bg-black padding-x padding-t pb-8">
+      <section className=' bg-black padding-x padding-t pb-8'>
         <Footer />
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
